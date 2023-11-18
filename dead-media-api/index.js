7f3d79ac707b98561ba3908362c067096160bf16
am679 <am679@pc7-100-l.cs.st-andrews.ac.uk>
2023-11-18 23:16:55 +0000

    Added delete endpoint

--- dead-media-api/index.js
+++ dead-media-api/index.js
@@ -6,11 +6,16 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 23720;
 app.use(express.json());
+
+const ids = new Set();
+
+//headers?
 
 const MediaStore = require('./store.js').MediaStore;
 const store = new MediaStore(false); //code must be robust against store suddenly going into error mode. 
+//try with true & skip past error parsing
 
 if (process.argv.length !== 3) {
     console.error('Usage is: node index.js <filepath>');
@@ -44,115 +49,99 @@ try {
 jsonData.forEach(mediaObject => {
     const { name, type, desc } = mediaObject;
     store.create(name, type, desc).then(createId => {
-        console.log(`Object added: ${createId}`);
+        console.log(`Object added: ${createId}`); //move to outside?
+        ids.add(createId);
     }).catch(error => {
         console.error('Error adding object');
         process.exit(1);
     });
 });
 
-// function formatObject(mediaObject, isArray) {
-//     if (isArray) {
-//         return mediaObject.map(media => {
-//             return {
-//                 id: `/media/${media.id}`, //conside removing "/media/"
-//                 name: media.name,
-//                 type: media.type,
-//                 desc: media.desc,
-//             };
-//         });
-//     } else {
-//         const formattedMediaObject = {
-//             id: `/media/${mediaObject.id}`,
-//             name: mediaObject.name,
-//             type: mediaObject.type,
-//             desc: mediaObject.desc,
-//         };
-//         return formattedMediaObject;
-//     }
-// }
+const formattedMediaObject = media => ({
+    id: `/media/${media.id}`,
+    name: media.name,
+    type: media.type,
+    desc: media.desc,
+});
 
-const formattedMediaObject = {
-    id: `/media/${mediaObject.id}`,
-    name: mediaObject.name,
-    type: mediaObject.type,
-    desc: mediaObject.desc,
-};
-
-app.get('/media', async (req, res) => { // /:id for specific objects
+app.get('/media', async (req, res) => {  //test with remove
     try {
-        const returnedObjects = await store.retrieveAll(); //store.retrieveAll().then(function (something) { if (something) { }})
-        if (returnedObjects) {
-            const formattedMediaObjects = returnedObjects.map(formattedMediaObject);
-            // formatObject(returnedObjects, true); //possible issue with accidentally using non-boolean values
-            res.status(200).json(formattedMediaObjects);
+        const returnedObjects = await store.retrieveAll(); 
+        if (ids.size === 0) {
+            res.status(204);
         } else {
-            res.status(204); //test this
+            const formattedObjects = returnedObjects.map(formattedMediaObject);
+            res.status(200).json(formattedObjects);
         }
     } catch (error) {
-        res.status(500).send(); //is this an empty body or is .json({}) better
+        res.status(500).send(); 
     }
-});
+}) //semi colon?
 
 app.get('/media/:id', async (req, res) => {
     try {
         const id = Number(req.params.id);
-        const returnedObject = await store.retrieve(id); //doesnt work for media/:5 since :5 cant be converted to number
-        if (returnedObject) {
-            const formattedMediaObject = formatObject(returnedObject, false);
-            res.status(200).json(formattedMediaObject);
-        } else {
-            res.status(404).send(); //does this work
+        if (!ids.has(id)) {
+            res.status(404).send();
+        }
+        else {
+            const returnedObject = await store.retrieve(id); //doesnt work for media/:5 since :5 cant be converted to number
+            const formattedObject = formattedMediaObject(returnedObject);
+            res.status(200).json(formattedObject);
         }
     } catch (error) {
-        res.status(500).send()
+        if (error === `Error: cannot find media with ID: ${req.params.id}`) { //check
+            res.status(404).send();
+        }
+        res.status(500).send();
     }
-});
+})
 
 app.post('/media', async (req, res) => {
     try {
         const { name, type, desc } = req.body; 
         //DataSet validaton
         const createId = await store.create(name, type, desc);
-        const newResource = await store.retrieve(createId);
-        if (newResource) { //conside removing
-            const formattedMediaObject = formatObject(newResource, false);
-            res.status(201).json(formattedMediaObject);
-        } else {
-            res.status(500).send();
-        }
+        ids.add(createId);
+        const newResource = await store.retrieve(createId); 
+        const formattedObject = formattedMediaObject(newResource);
+        res.status(201).json(formattedObject);
     } catch (error) {
         res.status(500).send();
     }
 })
 
-app.put('/media/:id', async (req, res) => { //can replace async and await with "then"
+app.put('/media/:id', async (req, res) => { 
     try {
         const id = Number(req.params.id);
         const { name, type, desc } = req.body;
 
-        const resourceExists = await store.retrieve(id);
-        if (!(resourceExists)) {
-            console.log("hi)");
-            return res.status(404).send();
-        }
-
-        await store.update(id, name, type, desc);
-        const updatedResource = await store.retrieve(id);
-        if (updatedResource) {
-            const formattedMediaObject = formatObject(updatedResource);
-            res.status(200).json(formattedMediaObject);
+        if (ids.has(id)) { //worth caching over using one promise? 
+            await store.update(id, name, type, desc);
+            const updatedResource = await store.retrieve(id);
+            const formattedObject = formattedMediaObject(updatedResource);
+            res.status(200).json(formattedObject);
         } else {
-            res.status(500).send();
-        }
-
-    } catch (error) {
-        if (error === `Error: cannot find media with ID: ${req.params.id}`) {
             res.status(404).send();
-        }
+        }       
+    } catch (error) {
         res.status(500).send();
     }
+})
 
+app.delete('/media/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        if (ids.has(id)) {
+            await store.delete(id);
+            res.status(204).send();
+            ids.delete(id);
+        } else {
+            res.status(404).send();
+        }
+    } catch (error) {
+        res.status(500).send();
+    }
 })
 
 app.listen(port, function () {
